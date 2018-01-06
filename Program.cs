d6bf3777e3d5471501b7a7a79e6abed7752804f1
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2018-01-06 23:17:01 +0600

    Implement strategy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DesignPatterns.Strategy.Core;
 
 namespace DesignPatterns.Strategy
 {
@@ -6,7 +7,18 @@ namespace DesignPatterns.Strategy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lets work with ideal duck");
+            Duck idealDuck = new Duck(new RealLook(), new FlySmothly(), new QuackLoudly());
+            idealDuck.Display();
+            idealDuck.Fly();
+            idealDuck.Quack();
+
+            System.Console.WriteLine("When duck is sick :(");
+            idealDuck.SetFly(new FlyHardly());
+            idealDuck.SetQuack(new QuackSliently());
+            idealDuck.Fly();
+            idealDuck.Quack();
+            Console.Read();
         }
     }
 }
